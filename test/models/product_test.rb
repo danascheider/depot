b6c5e564adeb01ef431a83418e836992a792fe65
Dana Scheider <dana.scheider@gmail.com>
2018-05-20 15:32:31 -0700

    Add fixtures and additional tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  def new_product image_url
+    Product.new(title: "Title", description: "yyy", image_url: image_url, price: 1)
+  end
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -8,5 +12,32 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
     assert product.errors[:description].any?
     assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "Title", description: "yyy", image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image URL" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.png.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} should be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end
   end
 end
